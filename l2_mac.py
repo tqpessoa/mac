c829fcd3b92bcf2d0ba4ec3e8a3b882a94d569e1
Talles Pessoa <eng.quintao@gmail.com>
2018-04-09 15:36:32 -0300

    Update l2_mac.py

--- l2_mac.py
+++ l2_mac.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Alterado: Talles Quintão
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
